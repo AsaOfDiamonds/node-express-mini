f4496fab86814ef8157eb69dea0db645e8323628
1@$@1github <asaofdiamonds@gmail.com>
2019-01-08 13:35:11 -0700

    added update, delete, and post

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const db = require('./data/db.js'); // CommonJS Modules
 
 const server = express();
 
-server.get('/', (req, res) => {
+// wire up global middleware
+server.use(express.json()); // teaches express how to parse json from the body
+
+server.get('/api/users', (req, res) => {
     db.find()
         .then(users => {
             res.status(200).json(users);
@@ -16,8 +19,8 @@ server.get('/', (req, res) => {
         });
 });
 
-server.get('/api/users/:userid', (req, res) => {
-    const id = req.params.userid;
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
 
     db.findById(id)
         .then(user => {
@@ -30,9 +33,74 @@ server.get('/api/users/:userid', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body; // reads information from the body of the request
+
+    db.insert(userInfo) // returns a promise, so we need to use .then
+        .then(result => {
+            db.findById(result.id)
+                .then(user => {
+                    res.status(201).json(user);
+                })
+                .catch(err =>
+                    res.status(500).json({ message: 'the get by id failed', error: err })
+                );
+        })
+        .catch(err =>
+            res.status(500).json({ message: 'the post failed', error: err })
+        );
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then(user => {
+            if (user) {
+                db.remove(id).then(count => {
+                    res.status(200).json(user);
+                });
+            } else {
+                res
+                    .status(404)
+                    .json({ message: 'The user with the specified ID does not exist.' });
+            }
+        })
+        .catch(err => res.status(500).json(err));
+});
+
+server.get('/users/first/:first/last/:last', (req, res) => {
+    res.send({ hello: `${req.params.first} ${req.params.last}` });
+});
+
+// /greet?first=kai&last=Lovingfoss
+server.get('/greet', (req, res) => {
+    const { first, last } = req.query;
+
+    res.send({ Greetings: `${first} ${last}` });
+});
+
+server.put('/api/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    try {
+        const result = await db.update(id, changes);
+
+        console.log('result', result);
+
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 server.listen(5000, () => console.log('server running'));
 
+//https://github.com/orgs/LambdaSchool/people
+
 // Cannot find module 'express' > npm i express || yarn add express
+// Cannot POST /api/user
 
 // client > server: Accept header (application/json)
 // server > client: Content-Type: (application/json; text/html)
